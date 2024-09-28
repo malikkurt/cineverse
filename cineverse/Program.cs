9a0dd63fca332a6f7c0e21571bc790f4e86b8cc4
malikkurt <malikkurt612@gmail.com>
2024-09-29 01:02:50 +0300

    Designing multi-tier architecture

--- cineverse/Program.cs
+++ cineverse/Program.cs
@@ -11,25 +11,25 @@ builder.Services.AddDbContext<MovieDbContext>(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<MovieDbContext>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var context = scope.ServiceProvider.GetRequiredService<MovieDbContext>();
 
-    var movie = new Movie()
-    {
-        MovieID = 0,
-        Title = "Ivýr Zývýr",
-        ReleaseDate = DateTime.Now,
-        Director = "Ivýretti",
-        Rating = 10,
-        Description = "Description",
-        PosterURL = "https://fma.fandom.com/wiki/Edward_Elric?file=213254+1407532169190+full.png",
-        TrailerURL = "https://www.youtube.com/watch?v=v1PciHjcsAk"
-    };
+//    var movie = new Movie()
+//    {
+//        MovieID = 0,
+//        Title = "Ivýr Zývýr",
+//        ReleaseDate = DateTime.Now,
+//        Director = "Ivýretti",
+//        Rating = 10,
+//        Description = "Description",
+//        PosterURL = "https://fma.fandom.com/wiki/Edward_Elric?file=213254+1407532169190+full.png",
+//        TrailerURL = "https://www.youtube.com/watch?v=v1PciHjcsAk" 
+//    };
 
-    context.Movies.Add(movie);
-    context.SaveChanges();
-}
+//    context.Movies.Add(movie);
+//    context.SaveChanges();
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
